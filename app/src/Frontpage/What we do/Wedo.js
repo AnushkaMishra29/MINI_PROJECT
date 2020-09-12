accf2dd6da4dcb70d8d8e7b6348337bc892bd7d1
Arnavjain1703 <arnavjainkht@gmail.com>
2020-09-13 00:29:03 +0530

    Responsive What we Do

--- app/src/Frontpage/What we do/Wedo.js
+++ app/src/Frontpage/What we do/Wedo.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './Wedo.css';
-class Wedo extends React.Component{
-    render()
-    {
-        return(
-            <div>
-               
-                <div className="section">
-                <div className='bgrnd'>
-                    <div className="pic"></div>
-                    <div className= 'txt'>lorem ipsum lorem isum jhgDQOG JHEGBDF JHGFEB</div>
+class Wedo extends React.Component {
+    state = {
+        para: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs Lorem ipsum, ipsum as it is sometime  print, graphic or web designs",
+        Images: [
+            "/assets/image2.jpg",
+            "/assets/image1.png"
+        ]
+
+
+    }
+    render() {
+        return ( 
+             <div className = "wedo">
+             <div className="web">   
+            <div className = "letter" ></div> 
+            <div className="row" >
+            <div className = "col-md-8 col-sm-12 font2" >
+            <div className="heading">WHAT WE DO</div>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs Lorem ipsum,
+            ipsum as it is sometime print, graphic or web designs 
+            </div> 
+            <div className = "col-md-4 col-sm-12" >
+             <img src={this.state.Images[1]}></img>
+            </div> 
+            </div> 
+            </div>
+            <div className="mobile">   
+            <div className="heading">WHAT WE DO</div>
+            <div className="row">
+                <div className="col-12">
+                <img src = {this.state.Images[1]}/>
                 </div>
+                <div className="col-12 font2">
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs Lorem ipsum,
+            ipsum as it is sometime print, graphic or web designs 
                 </div>
+            </div>
+            </div>
             </div>
 
         )
